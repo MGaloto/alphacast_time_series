a4ac14503a63744549431f59039451f84de6d595
MGaloto <maxigaloto@hotmail.com>
2022-01-31 21:04:56 -0300

    readme

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# alphacast_time_series
-Web Scraping + Transform data + Graph Time Series for the alphacast platform
+<p align="center">
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/200px-Python-logo-notext.svg.png" alt="react" width="50" height="50" />
+</p>
+
+
+
+# Alphacast.io
+
+
+<p align="center">
+<img src="https://www.alphacast.io/images/alphacast.svg" alt="react" width="300" height="150" />
+</p>
+
+
+
+Alphacast es una start-up en crecimiento que se encuentra construyendo una plataforma para facilitar el análisis económico y financiero. Think GitHub for Economists. Recopila información de cientos de fuentes de datos. Oficinas de Estadística, Bancos Centrales, Tesorería. Mercados y fuentes privadas. Ajuste estacional, suavizado, filtrado e interpolación, % PIB, conversión de divisas, precios constantes, YoY, MoM, WoW, medias móviles y mucho más.
+
+
+## Web Scraping + Transform + Load Data
+
+
+En este trabajo se transforman las series temporales de los principales indices bursátiles internacionales a base 100 en enero de 2007 hasta el 31/01/2022. La idea principal del trabajo es poder comparar las series de tiempo principalmente durante los dos shock que se vivieron en el periodo analizado, la crisis subprime y el Covid-19.
+
+
+
+# Librerias Utilizadas
+
+
+<ui>
+
+<li>
+{matplotlib}
+</li>
+
+<li>
+{pandas_datareader}
+</li>
+
+<li>
+{Pandas}
+</li>
+
+<li>
+{alphacast}
+</li>
+
+</ui>
+
+
+
+# Graficos final:
+
+Películas:
+
+![.](plots/plot1.png)
+
+
+
+
+
--- index_time_series.py
+++ index_time_series.py
@@ -1,13 +1,18 @@
 
-import math
+
+# Importando librerias y ordenando el conjunto de datos
+
+
+
 import pandas_datareader as web
 import numpy as np
-import pandas as np
+import pandas as pd
 import matplotlib.pyplot as plt
+import datetime
 plt.style.use('fivethirtyeight')
 
 
-#S&P 500, nasdaq composite, IBEX 35, DAX, Nikkei ,Dow Jones, IBOVESPA, IPC MEXICO, HANG SENG INDEX 
+
 
 indices = ['^GSPC', '^IXIC', '^IBEX', '^GDAXI', '^N225', '^DJI', '^BVSP', '^MXX','^HSI']
 
@@ -15,13 +20,8 @@ df = web.DataReader(indices, data_source='yahoo',
                     start='2007-01-01',
                     end='2022-01-31')['Close']
 
-
-df.columns = ['S&P_500', 'Nasdaq', 'IBEX_35', 'DAX', 'Nikkei' ,'Dow_Jones', 'BOVESPA', 'IPC_Mexico', 'HANG_SENG_INDEX']
-
-
-
-
 #%%
+
 
 # El metodo de ffill, front filling, significa relleno frontal, le asigna al valor faltante el valor del periodo posterior, muy util para series temporales. El otro metodo es el bfill donde usamos el periodo previo.
 
@@ -31,3 +31,80 @@ df= df.fillna(method='bfill')
 
 
 #%%
+
+# Transofmacion del data set base 100 
+
+df = df.div(df.iloc[0]).mul(100)
+
+
+ 
+df = df.round(decimals=2)
+
+df['date'] =  df.index
+
+df['date'] = df['date'].dt.date
+
+
+df.columns = ['S&P_500', 'Nasdaq', 'IBEX_35', 'DAX', 'Nikkei' ,'Dow_Jones', 'BOVESPA', 'IPC_Mexico', 'HANG_SENG_INDEX', 'Date']
+
+
+df.index = np.arange(0, len(df))
+
+
+df['Date'] = pd.to_datetime(df.Date).dt.strftime('%Y-%m-%d')
+
+
+
+#%%
+
+
+# Api Key Alphacast
+
+with open('C:/Users/maxig/Desktop/Carpetas/Trabajos en Python/Alphacast/New Work/Key.txt') as claves: keys = [clave for clave in claves]
+print(keys)
+
+key = keys[0]
+
+# Iniciando Alphacast
+
+from alphacast import Alphacast
+
+alphacast = Alphacast(key)
+
+
+
+#%%
+
+
+alphacast.repository.read_all()
+
+
+alphacast.datasets.create('Indices Bursatiles desde 2007', 1272, 'Principales indices bursatiles del mundo con base 100 en enero de 2007 hasta el 31/01/2022')
+
+
+#%%
+
+df['country'] = 'Index'
+
+df = df[['Date', 'country', 'S&P_500', 'Nasdaq', 'IBEX_35', 'DAX', 'Nikkei' ,'Dow_Jones', 'BOVESPA', 'IPC_Mexico', 'HANG_SENG_INDEX']]
+
+#%%
+
+alphacast.datasets.dataset(14940).initialize_columns(dateColumnName = "Date", 
+                                                            entitiesColumnNames=['country'], 
+                                                            dateFormat= "%Y-%m-%d")
+
+#%%
+
+alphacast.datasets.dataset(14940).upload_data_from_df(df,
+                                                      deleteMissingFromDB = False,
+                                                      onConflictUpdateDB = False,
+                                                      uploadIndex=False)
+
+#%%
+
+alphacast.datasets.dataset(14940).processes()
+
+
+#%%
+
